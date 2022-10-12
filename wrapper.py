3de461edae72cc9a0cf71fd65909285ee427c33b
Rene Haas <renha@itu.dk>
2022-10-12 22:21:16 +0200

    added start from

--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,28 @@
+from sched import scheduler
 import torch
+
 from transformers import CLIPTextModel, CLIPTokenizer
-from diffusers import AutoencoderKL, UNet2DConditionModel, LMSDiscreteScheduler
-from tqdm.auto import tqdm
-from torch import autocast
+from diffusers import (
+    AutoencoderKL, 
+    UNet2DConditionModel, 
+    LMSDiscreteScheduler,
+    DDIMScheduler)
+from tqdm import tqdm
+from torch import autocast, inference_mode
 import PIL
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy
 
 
-# For video display:
 from IPython.display import HTML
 from base64 import b64encode
 
 import torchvision.transforms as T
+
+from transformers import logging
+
+logging.set_verbosity_error()
 
 
 def tensor_to_pil(tensor_imgs):
@@ -29,13 +38,18 @@ def pil_to_tensor(pil_imgs):
         tensor_imgs = to_torch(pil_imgs).unsqueeze(0) * 2 - 1
     elif type(pil_imgs) == list:
         tensor_imgs = torch.cat(
-            [to_torch(pil_imgs).unsqueeze(0) * 2 - 1 for img in pil_imgs])
+            [to_torch(pil_imgs).unsqueeze(0) * 2 - 1 for img in pil_imgs]
+        )
     else:
         raise Exception("Input need to be PIL.Image or list of PIL.Image")
     return tensor_imgs
 
 
 def image_grid(imgs, rows=1, cols=1):
+# n = 10
+# num_rows = 4
+# num_col = n // num_rows
+# num_col  = num_col + 1 if n % num_rows else num_col
     if type(imgs) == torch.Tensor:
         imgs = tensor_to_pil(imgs)
     assert len(imgs) >= rows * cols
@@ -48,12 +62,15 @@ def image_grid(imgs, rows=1, cols=1):
 
 
 class StableDifussionWrapper:
-    def __init__(self, width=512, height=512):
+    def __init__(self, 
+                img_size = (512,512),
+                scheduler_type = "lsm",
+                ):
         # Set device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        self.height = width
-        self.width = height
+        self.height, self.width = img_size
+        self.scheduler_type = scheduler_type
 
         # Load the autoencoder model which will be used to decode the latents into image space.
         self.vae = AutoencoderKL.from_pretrained(
@@ -72,14 +89,7 @@ class StableDifussionWrapper:
             "CompVis/stable-diffusion-v1-4", subfolder="unet", use_auth_token=True
         ).to(self.device)
 
-        # The noise scheduler
-        self.scheduler = LMSDiscreteScheduler(
-            beta_start=0.00085,
-            beta_end=0.012,
-            beta_schedule="scaled_linear",
-            num_train_timesteps=1000,
-        )
-
+        self.set_scheduler()
         # Prompt engeneering
         self.pe_words = [
             "digital art",
@@ -93,14 +103,31 @@ class StableDifussionWrapper:
             "8k",
         ]  # " "nikon d3300:25"]
 
+    def set_scheduler(self, scheduler_type = None):
+        if not scheduler_type is None:
+            self.scheduler_type = scheduler_type
+        
+        if self.scheduler_type == "ldm":
+            # The noise scheduler
+            self.scheduler = LMSDiscreteScheduler(
+                beta_start=0.00085,
+                beta_end=0.012,
+                beta_schedule="scaled_linear",
+                num_train_timesteps=1000,
+            )
+        elif self.scheduler_type == "ddim":
+            self.scheduler = DDIMScheduler(
+                beta_start=0.00085, beta_end=0.012,
+                beta_schedule='scaled_linear', num_train_timesteps=1000)
+        else:
+            raise NotImplementedError
     def encode_image(self, imgs):
-
         # Single image -> single latent in a batch (so size 1, 4, 64, 64)
         with torch.no_grad():
             latents = self.vae.encode(imgs)
         return 0.18215 * latents.mode()  # or .mean or .sample
 
-    def decode_latents(self, latents):
+    def decode(self, latents):
         # scale and decode the image latents with vae
         latents = 1 / 0.18215 * latents
         with torch.no_grad():
@@ -126,35 +153,51 @@ class StableDifussionWrapper:
 
         if latents is None:
             latents = self.random_latents(seed=seed)
+        
+        
+        # if start_step > 0:
+        start_timestep = self.scheduler.timesteps[start_step].long()
+        # start_timesteps = start_timestep
+        noise = torch.randn_like(latents)
+        start_timesteps = start_timestep.repeat(latents.shape[0]).long()
 
-        latents = latents * self.scheduler.sigmas[start_step]
+        latents = self.scheduler.add_noise(latents, noise, start_timesteps)
 
-        with autocast("cuda"):
-            for i, t in tqdm(enumerate(self.scheduler.timesteps)):
-                if i >= start_step:
-                    # expand the latents if we are doing classifier-free
-                    # guidance to avoid doing two forward passes.
-                    latent_model_input = torch.cat([latents] * 2)
+        if self.scheduler_type == "ldm":
+            #latents = latents * self.scheduler.sigmas[start_step-1 if start_step>0 else start_step]
+            latents = latents * self.scheduler.sigmas[start_step]
+            
+        with autocast("cuda"), inference_mode():
+            for i, t in tqdm(enumerate(self.scheduler.timesteps[start_step:]),leave = False):
+            
+                # expand the latents if we are doing classifier-free
+                # guidance to avoid doing two forward passes.
+                latent_model_input = torch.cat([latents] * 2)
+                
+                if self.scheduler_type == "ldm":
                     sigma = self.scheduler.sigmas[i]
                     latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
 
-                    # predict the noise residual
-                    with torch.no_grad():
-                        noise_pred = self.unet(
-                            latent_model_input,
-                            t,
-                            encoder_hidden_states=self.prep_text(prompt),
-                        )["sample"]
+                # predict the noise residual
+                with torch.no_grad():
+                    noise_pred = self.unet(
+                        latent_model_input,
+                        t,
+                        encoder_hidden_states=self.prep_text(prompt),
+                    )["sample"]
 
-                    # perform classifier-free guidance
-                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred = noise_pred_uncond + guidance_scale * (
-                        noise_pred_text - noise_pred_uncond
-                    )
+                # perform classifier-free guidance
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (
+                    noise_pred_text - noise_pred_uncond
+                )
+                # compute the previous noisy sample x_t -> x_t-1
 
-                    # compute the previous noisy sample x_t -> x_t-1
-                    latents = self.scheduler.step(noise_pred, i, latents)["prev_sample"]
-            return latents
+                if self.scheduler_type == "ldm":
+                    latents = self.scheduler.step(noise_pred, i, latents)["prev_sample"]                        
+                if self.scheduler_type == "ddim":
+                    latents = self.scheduler.step(noise_pred, t, latents)["prev_sample"]
+        return latents
 
     def add_pe_words(self, prompt):
         return prompt + ", " + ", ".join(self.pe_words)
@@ -195,5 +238,25 @@ class StableDifussionWrapper:
         print(self.encode_text(prompt).shape)
         print(self.prep_text(prompt).shape)
         print(self.random_latents().shape)
-        print(self.decode_latents(self.random_latents()).shape)
+        print(self.decode(self.random_latents()).shape)
         print(self.add_noise(self.random_latents()).shape)
+
+    def generate_images(self, prompt,
+                        n = 4,
+                        seed=None, 
+                        num_inference_steps=40,  # More steps better quality but slower, 30-40 is usually a good zone
+                        guidance_scale=3,  # Classifier-free guidance. Experiement with this value to affect image quality
+                        ):
+        torch.manual_seed(seed)
+        all_imgs = []
+        for i in tqdm(range(n)):
+            latents = self.diffusion_loop(
+                prompt=prompt,
+                seed=None,  # change seed for different images or use `None` for random ones
+                num_inference_steps=num_inference_steps,  # More steps better quality but slower, 30-40 is usually a good zone
+                guidance_scale=guidance_scale,  # Classifier-free guidance. Experiement with this value to affect image quality
+            )
+            imgs = self.decode(latents)
+            all_imgs.append(imgs)
+        all_imgs = torch.cat(all_imgs)
+        return all_imgs
